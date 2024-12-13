b544f60631c58cbcd865b07a0e795777a129b160
Adrien Cater <adrien@boring.ch>
2024-12-13 17:10:46 +0100

    V1

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# [Webrings](https://en.wikipedia.org/wiki/Webring) are social networks owned by the participants.
+# [Webrings](https://en.wikipedia.org/wiki/Webring) are social networks made by the users.
+
+This webring uses pages made on [Glitch: The friendly community where everyone builds the web](https://glitch.com)
+
+
+You can see an example page at [tricky-jumpy-powder.glitch.me](https://tricky-jumpy-powder.glitch.me/)
+
+The example playlist is at [glitch.com/@adriencateracom/webring-test](https://glitch.com/@adriencateracom/webring-test)
+
+# Instructions
+
+# 1. Make a hub
+
+Make a ‘Web Ring’ host page on glitch.
+
+This is the ‘hub’ of the web ring.
+
+You can use the **`[Import from GitHub]`** button on glitch.com to clone this repo. (Or put thes files in a glitch project by hand.)
+
+### 2. Note the address
+
+In the preview window, **`[Copy Link]`**
+
+The address will be something like **`https://some-funny-words.glitch.me/`**
+
+You can change this in the projet settings.
+
+# 3. Make a playlist
+
+Make a Playlist on glitch.
+
+Go to your project's page in **`Settings`** → **`Go to project page [↗︎]`** and click on the **`[Add to playlist]`** button. Add to a new playlist.
+
+### 4. Note the address
+
+The address of the paylist will be something like **`https://glitch.com/@username/playlistname`**
+
+# 5. Configure the javascript
+
+Open webring.js and edit the lines
+
+	const glitchPlaylist = "https://glitch.com/@username/playlistname";
+
+	const glitchWebRingPage = "https://some-funny-words.glitch.me";
+
+	const webRingTitle = "Test Ring";
+
+`glitchPlaylist` is the address of your playlist
+
+`glitchWebRingPage` is the address of your hub
+
+`webRingTitle` is the title of your web ring
+
+### 6. Add more projects to your playlist
+
+Invite some friends to make some cool stuff and add their projects to the playlist.
+
+# 7. Include the snippet in each page of the ring
+
+Every project needs to incude the following snippet of code in their index.html:
+
+	<!-- Webring -->
+	<div id="Webring"></div>
+	<script src="https://some-funny-words.glitch.me/webring.js"></script>
+	<!-- Webring -->
+
+Replace `some-funny-words` with your hub address
--- webring.js
+++ webring.js
@@ -1,1 +1,68 @@
-/* webring javascript */
+// CONFIG
+
+const glitchPlaylist = "https://glitch.com/@adriencateracom/webring-test";
+const glitchWebRingPage = "https://tricky-jumpy-powder.glitch.me";
+const webRingTitle = "Test Ring";
+
+// - - - - - - - - - - -
+
+// base glitch API request url:
+const glitchAPIbaseurl = "https://api.glitch.com/v1/collections/by/fullUrl/projects?fullUrl=";
+const dataString = glitchPlaylist.replace('https://glitch.com/@', '');
+const dataUrl = glitchAPIbaseurl + dataString;
+console.log("data source: " + dataUrl);
+
+const theList = []; // Initialize an empty list to store the "domain" values
+
+async function fetchData() {
+	try {
+		const response = await fetch(dataUrl); // Fetch data from the URL
+		
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
+
+		const data = await response.json();
+		
+		data.items.forEach(item => {
+			theList.push(item.domain);
+		});
+
+		//console.log('The List of Domains:', theList);
+
+		const currentHostname = window.location.hostname; // e.g. "hello.glitch.me"
+		const currentSubdomain = currentHostname.replace('.glitch.me', ''); // e.g. "hello"
+		
+		const currentIndex = theList.indexOf(currentSubdomain);
+
+		if (currentIndex !== -1) {
+			const prevIndex = (currentIndex - 1 + theList.length) % theList.length;
+			const nextIndex = (currentIndex + 1) % theList.length;
+
+			const prevLink = `https://${theList[prevIndex]}.glitch.me/`;
+			const nextLink = `https://${theList[nextIndex]}.glitch.me/`;
+
+			console.log('Previous Link:', prevLink);
+			console.log('Next Link:', nextLink);
+
+			// Build webring nav
+			var navContents = "";
+			navContents += '\n<link  rel="stylesheet" href="' + glitchWebRingPage + '/webring.css">';
+			navContents += '\n<a href="' + prevLink + '"><span>Prev</span></a>';
+			navContents += '\n<a href="' + glitchWebRingPage + '">' + webRingTitle + '</a>';
+			navContents += '\n<a href="' + nextLink + '"><span>Next</span></a>';
+			const ringNav = document.getElementById('Webring');
+			ringNav.innerHTML = navContents;
+		
+		} else {
+			console.log('This domain is not in the playlist.');
+		}
+
+	} catch (error) {
+		console.error('Error fetching data:', error);
+	}
+}//fetchData()
+
+window.onload = function(){
+	fetchData();
+};//window.onload
